f1c6efe77a6cee7e988869245310cc62ef387faa
Matt <matthewmccollum17@gmail.com>
2021-08-18 23:29:18 -0400

    Created initial page views with dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,102 @@
-from django.shortcuts import render, HttpResponse
+from datetime import date
 
-items = [
-    {'id': 1, 'title': 'Brave New World', 'author': 'Frank West'},
-    {'id': 2, 'title': 'Gone with the Wind', 'author': 'Swimming Trunks'},
-    {'id': 3, 'title': 'Ender\'s Game', 'author': 'Keyboard'}
+from django.shortcuts import render
+
+posts = [
+    {
+        "slug": "swimming-in-the-sea",
+        "image": "dolphins.jpg",
+        "author": "Matt",
+        "date": date(2021, 8, 18),
+        "title": "Dolphin Diving",
+        "excerpt": "There's nothing like swimming with one of the world's smartest creatures to make you feel alive",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+        """
+    },
+    {
+        "slug": "the-best-ideas",
+        "image": "idea.jpg",
+        "author": "Matt",
+        "date": date(2021, 8, 15),
+        "title": "Idea Iteration",
+        "excerpt": "Think smarter, not harder. Just keep swimming. Finding Nemo is one of the best Pixar movies",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+        """
+    },
+    {
+        "slug": "road-tripping",
+        "image": "road-trip.jpg",
+        "author": "Matt",
+        "date": date(2021, 8, 10),
+        "title": "Country Roads",
+        "excerpt": "Take me home. John Denver is one the classic country artists. He liked guitars.",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, id! Magni,
+            ullam perspiciatis eligendi placeat quos iste labore libero, est sunt beatae
+            et, repudiandae maiores. Ab, perferendis? Rerum, mollitia aperiam!
+        """
+    }
 ]
 
 
-def index(request):
-    return render(request, 'blog/index.html')
+def starting_page(request):
+    sorted_posts = sorted(posts, key=lambda post: post['date'])
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html', {
+        'posts': latest_posts
+    })
 
 
-def posts(request):
-    context = {'posts': items}
-    return render(request, 'blog/posts.html', context)
+def posts_page(request):
+    return render(request, 'blog/all-posts.html', {
+        'all_posts': posts
+    })
 
 
-def post(request, slug):
-    context = {'id': slug}
-    return render(request, 'blog/post.html', context)
+def post_detail(request, slug):
+    selected_post = next(post for post in posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        'post': selected_post
+    })
--- my_site/settings.py
+++ my_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-unay!gf28@k&vy^vd*9i&ysf2jniop=-6*-5_yt+fkoar)@y5r'
+SECRET_KEY = 'this-key-doesnt-work'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
